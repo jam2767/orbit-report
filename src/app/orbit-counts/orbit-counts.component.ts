7c74b8b8871d6820febf9a5ad34a291440dd9e93
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2020-12-15 02:15:15 -0600

    added orbit-counts component to show total number of satellites and satellites by type

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+//import { count } from 'console';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+  countSatelliteType(type: string): number{
+    let count = 0;
+    for(let i=0; i < this.satellites.length; i++){
+      if(this.satellites[i].type.toLowerCase() === type.toLowerCase()){ 
+      count++;
+    }
+  }
+  return count;
+  }
+}
